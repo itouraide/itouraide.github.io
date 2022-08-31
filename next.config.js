89c135ab481adc1b69a5c081f085506130d489fb
iafine <iafineden@gmail.com>
2022-08-31 19:11:29 +0800

    fix: 修复export未生成图片

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 module.exports = nextConfig
